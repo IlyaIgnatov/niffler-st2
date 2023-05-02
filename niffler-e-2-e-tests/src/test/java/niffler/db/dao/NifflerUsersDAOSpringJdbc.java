532aa8be9e20d5b74f6f6ada686fdfe77e69a0ee
ilya <ilya-681@ya.ru>
2023-05-02 13:09:30 +0300

    Prepaered create method

--- niffler-e-2-e-tests/src/test/java/niffler/db/dao/NifflerUsersDAOSpringJdbc.java
+++ niffler-e-2-e-tests/src/test/java/niffler/db/dao/NifflerUsersDAOSpringJdbc.java
@@ -5,15 +5,24 @@ import niffler.db.ServiceDB;
 import niffler.db.entity.UserEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
 import java.util.UUID;
 
 public class NifflerUsersDAOSpringJdbc implements NifflerUsersDAO {
 
   private final TransactionTemplate transactionTemplate;
   private final JdbcTemplate jdbcTemplate;
+  private static final PasswordEncoder pe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
 
   public NifflerUsersDAOSpringJdbc() {
     DataSourceTransactionManager transactionManager = new JdbcTransactionManager(
@@ -24,7 +33,47 @@ public class NifflerUsersDAOSpringJdbc implements NifflerUsersDAO {
 
   @Override
   public int createUser(UserEntity user) {
-    return 0;
+
+
+    return transactionTemplate.execute(st -> {
+      KeyHolder keyHolder = new GeneratedKeyHolder();
+
+      int st1 = jdbcTemplate.update("INSERT INTO users "
+                      + "(username, password, enabled, account_non_expired, account_non_locked, credentials_non_expired) "
+                      + " VALUES (?, ?, ?, ?, ?, ?)",
+              user.getUsername(),
+              pe.encode(user.getPassword()),
+              user.getEnabled(),
+              user.getAccountNonExpired(),
+              user.getAccountNonLocked(),
+              user.getCredentialsNonExpired(),
+              keyHolder
+      );
+
+      final UUID createdUserId;
+
+      if (keyHolder.getKeys().size() > 1) {
+        createdUserId = UUID.fromString((String)keyHolder.getKeys().get("id"));
+      } else {
+        createdUserId= UUID.fromString(keyHolder.getKey().toString());
+      }
+
+
+      String insertAuthoritiesSql = "INSERT INTO authorities (user_id, authority) VALUES ('%s', '%s')";
+
+      List<String> sqls = user.getAuthorities()
+              .stream()
+              .map(ae -> ae.getAuthority().name())
+              .map(a -> String.format(insertAuthoritiesSql, createdUserId, a))
+              .toList();
+
+      int updateRows=0;
+      for (String sql_st : sqls) {
+        updateRows += jdbcTemplate.update(sql_st);
+      }
+
+      return updateRows;
+    });
   }
 
   @Override
@@ -50,11 +99,11 @@ public class NifflerUsersDAOSpringJdbc implements NifflerUsersDAO {
     );
   }
 
-  @Override
+  /*@Override
   public int removeUser(UserEntity user) {
     return transactionTemplate.execute(st -> {
       jdbcTemplate.update("DELETE FROM authorities WHERE user_id = ?", user.getId());
       return jdbcTemplate.update("DELETE FROM users WHERE id = ?", user.getId());
     });
-  }
+  }*/
 }
